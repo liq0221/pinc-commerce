5d9bf75a9346ef8a7d3f61897a39842b9ccddd15
liqian <469363040@qq.com>
2021-11-15 00:26:04 +0800

    feat():add

---
+++ pinc-commerce-gateway/src/main/java/com/pinc/commerce/filter/HeaderTokenGatewayFilter.java
@@ -0,0 +1,32 @@
+package com.pinc.commerce.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * http头部携带token过滤器
+ */
+public class HeaderTokenGatewayFilter implements GatewayFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        String name = exchange.getRequest().getHeaders().getFirst("token");
+
+        if ("pinc".equals(name)) {
+            return chain.filter(exchange);
+        }
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        return exchange.getResponse().setComplete();
+    }
+
+    @Override
+    public int getOrder() {
+        return HIGHEST_PRECEDENCE + 2;
+    }
+}
---
+++ pinc-commerce-gateway/src/main/java/com/pinc/commerce/filter/factory/HeaderTokenGatewayFilterFactory.java
@@ -0,0 +1,14 @@
+package com.pinc.commerce.filter.factory;
+
+import com.pinc.commerce.filter.HeaderTokenGatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HeaderTokenGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
+    @Override
+    public GatewayFilter apply(Object config) {
+        return new HeaderTokenGatewayFilter();
+    }
+}
